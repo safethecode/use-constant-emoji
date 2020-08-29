076e1d7838e70eadda560a07206c8a147e20a861
0xBono <iam@Sonjim.in>
2020-08-29 14:29:27 +0900

    chore: added example code :children_crossing:

---
+++ example/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {ALARM_CLOCK} from 'use-constant-emoji'
+
+function App() {
+  return (
+    <div className="App">
+      <span>{ALARM_CLOCK}</span>
+    </div>
+  );
+}
+
+export default App;
